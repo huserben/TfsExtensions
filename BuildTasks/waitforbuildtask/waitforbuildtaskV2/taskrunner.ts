8d4f3ba7391d643a9bf78661b181b189eb62869f
Benji Huser <benj.huser@gmail.com>
2021-12-20 18:18:42 +0100

    Revert Update Cancel Build Task

--- BuildTasks/waitforbuildtask/waitforbuildtaskV2/taskrunner.ts
+++ BuildTasks/waitforbuildtask/waitforbuildtaskV2/taskrunner.ts
@@ -41,7 +41,7 @@ export class TaskRunner {
             await this.parseInputs();
 
             await this.waitForBuildsToFinish(this.triggeredBuilds);
-        } catch (err: any) {
+        } catch (err) {
             this.taskLibrary.setResult(tl.TaskResult.Failed, err.message);
         } finally {
             if (this.clearVariable === true) {
